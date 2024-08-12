d0918f1fa1bbbad59976a910b1ba6bbb38b7413e
abner G Jacobsen <abner@apoana.com.br>
2024-08-12 14:12:03 -0300

    feat(examples): add basic settings example for application configuration
    fix(mkdocs.yml): correct path to custom CSS file for proper styling
    
    docs(mkdocs.yml): update versioning configuration to include latest and dev

---
+++ examples/settings_basic.py
@@ -0,0 +1,39 @@
+from functools import lru_cache
+from snapenv_core.settings.manager import SnapEnvCommonSettings
+
+
+class AppSettings(SnapEnvCommonSettings):
+    """
+    Application settings configuration.
+
+    Attributes
+    ----------
+    APP_TITLE : str
+        The title of the application.
+    LOG_LEVEL : str
+        The log level for the application.
+    """
+
+    APP_TITLE: str
+    LOG_LEVEL: str
+
+
+@lru_cache
+def get_settings() -> AppSettings:
+    """
+    Retrieve the application settings with caching.
+
+    This function uses an LRU cache to store the settings so that
+    subsequent calls are fast and do not re-initialize the settings.
+
+    Returns
+    -------
+    AppSettings
+        The application settings instance.
+    """
+    return AppSettings()
+
+
+settings: AppSettings = get_settings()
+
+print(settings)
